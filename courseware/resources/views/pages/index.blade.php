7d2331f935b70f97fafdda09aebff66282dbd73c
jamping882 <hannajane@gmail.com>
2020-12-21 22:35:51 +0800

    first commit

---
+++ courseware/resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('base')
+
+@section('content')
+
+<h1 class="text-white">Welcome!</h1>
+<br>
+<p class="text-white">
+    This is my sample web application powered by Laravel<sup>TM</sup>.
+</p>
+
+@stop
